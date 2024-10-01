ee1dbcf55c32f227343c5ffb0687e8664b4ce34e
Jerry Vogel <vogel.jer@northeastern.edu>
2024-10-01 15:35:00 -0400

    Fixing merge conflicts

--- src/pages/components/profile.tsx
+++ src/pages/components/profile.tsx
@@ -1,4 +1,4 @@
-export default function Profile() {s
+export default function Profile() {
   return (<img
       src="https://i.redd.it/a-compilation-of-sad-sopping-wet-cats-v0-ojzedsm0vow81.jpg?width=1080&format=pjpg&auto=webp&s=c7f8966cea95b11d907181b39761b46d46f34bd9"
       alt="A sad little creature"
